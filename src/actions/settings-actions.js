528d45357e06950a4ae53e373e8005bb108a3c04
ulises-jeremias <ulisescf.24@gmail.com>
2019-02-16 02:12:24 -0300

    Added settings module

--- src/actions/settings-actions.js
+++ src/actions/settings-actions.js
@@ -10,7 +10,29 @@ import {
   SETTINGS_STORED_CONFIGURATION_GET_REQUEST,
   SETTINGS_STORED_CONFIGURATION_GET_SUCCESS,
   SETTINGS_STORED_CONFIGURATION_GET_FAILURE,
+
+  ON_SETTINGS_FORM_CLEAR,
+  ON_SETTINGS_FORM_FIELD_CHANGE,
 } from '../constants'
+
+exports.onLicensesFormClear = (context, options) => {
+  options = options || { error: true, success: true }
+
+  return {
+    type: ON_SETTINGS_FORM_CLEAR,
+    payload: {
+      context,
+      options,
+    },
+  }
+}
+
+exports.onLicensesFormFieldChange = (context, field, value, currentUpdate = false) => {
+  return {
+    type: ON_SETTINGS_FORM_FIELD_CHANGE,
+    payload: { context, field, value, currentUpdate },
+  }
+}
 
 exports.getStoredConfiguration = () => {
   return dispatch => {
--- src/reducer/settings-reducer.js
+++ src/reducer/settings-reducer.js
@@ -1,9 +1,83 @@
-import { handleActions } from 'redux-actions'
+import { combineActions, handleActions } from 'redux-actions'
+
+import {
+  SETTINGS_STORED_CONFIGURATION_GET_REQUEST,
+  SETTINGS_STORED_CONFIGURATION_GET_SUCCESS,
+  SETTINGS_STORED_CONFIGURATION_GET_FAILURE,
+  
+  ON_SETTINGS_FORM_CLEAR,
+  ON_SETTINGS_FORM_FIELD_CHANGE,
+} from '../constants'
 
 import initialState from '../state/settings-state'
+import formValidation from './settings-form'
+
+import {
+  requestHandler,
+  successHandler,
+  failureHandler,
+} from '../helpers/action-handler'
 
 const reducer = handleActions({
-    
+  [combineActions(
+    SETTINGS_STORED_CONFIGURATION_GET_REQUEST,
+  )]: requestHandler('settings'),
+
+  [combineActions(
+    SETTINGS_STORED_CONFIGURATION_GET_SUCCESS,
+  )]: successHandler('settings'),
+
+  [combineActions(
+    SETTINGS_STORED_CONFIGURATION_GET_FAILURE,
+  )]: failureHandler('settings'),
+
+  [ON_SETTINGS_FORM_CLEAR]: (state, { payload: { context, options } }) => {
+    let nextSettingsState = {
+      ...state,
+      [context]: {
+        ...initialState[context],
+        isValid: false
+      },
+    }
+
+    Object.keys(options || {})
+      .filter(option => options[option])
+      .forEach(option => {
+        nextSettingsState[context][option] = initialState[context][option]
+      })
+
+    return nextSettingsState
+  },
+
+  [ON_SETTINGS_FORM_FIELD_CHANGE]: (state, action) => {
+    const {
+      context,
+      field,
+      value,
+      currentUpdate,
+    } = action.payload
+
+    const contextData = state[context]
+
+    let nextSettingsState = {
+      ...state,
+      [context]: {
+        ...contextData,
+        error: null,
+        success: null,
+        current: !currentUpdate ? contextData.current : {
+          ...contextData.current,
+          [field]: value,
+        },
+        fields: {
+          ...contextData.fields,
+          [field]: value,
+        },
+      }
+    }
+
+    return formValidation(context, nextSettingsState, action)
+  },
 }, initialState)
   
 export default reducer
